05e5c0c59ba60b01a731a7434eb777e3464a4d38
Daniel Castillo <ec1335784@gmail.com>
2022-04-20 22:44:54 -0600

    add two new activities

--- app/src/main/java/com/example/parcialndos/MainActivity.kt
+++ app/src/main/java/com/example/parcialndos/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.parcialndos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.info -> {
+                val intent = Intent(this, about_us::class.java).apply {
+
+                }
+                startActivity(intent)
+                true
+            }
+            R.id.contacto-> {
+                val intent = Intent(this, Contacto::class.java).apply {
+
+                }
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
