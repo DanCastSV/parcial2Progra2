d6a8937e815e2d5e883c9f1e8b38633cf4742737
Daniel Castillo <ec1335784@gmail.com>
2022-04-20 21:14:19 -0600

    add the splashscreen

---
+++ app/src/main/java/com/example/parcialndos/SplashActivity.kt
@@ -0,0 +1,50 @@
+package com.example.parcialndos
+
+import android.app.ActionBar
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import androidx.core.os.postDelayed
+import kotlinx.android.synthetic.main.activity_splash.*
+
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        val splashanimation = AnimationUtils.loadAnimation(this,R.anim.splash)
+        img_vet.startAnimation(splashanimation)
+
+
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >=Build.VERSION_CODES.R){
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+
+        }
+        else{
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        @Suppress("DEPRECATION")
+        Handler().postDelayed(
+            {
+
+                startActivity(
+                     Intent(this@SplashActivity, MainActivity::class.java)
+
+                )
+                finish()
+
+            },1000
+
+        )
+    }
+
+}
