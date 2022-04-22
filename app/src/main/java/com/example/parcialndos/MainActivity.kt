4cc4518b78b8ac6f622dbc5879df1d4c417c2bf0
Daniel Castillo <ec1335784@gmail.com>
2022-04-22 00:03:13 -0600

    add the sharedpreferences and mod the splash screen

--- app/src/main/java/com/example/parcialndos/MainActivity.kt
+++ app/src/main/java/com/example/parcialndos/MainActivity.kt
@@ -3,14 +3,59 @@ package com.example.parcialndos
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
+import android.preference.PreferenceManager
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.progressindicator.BaseProgressIndicator
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val save:Button=btnSave
+        val show:Button=btnShow
+        @Suppress("DEPRECATION")
+        val prefs = PreferenceManager.getDefaultSharedPreferences(this)
+
+        save.setOnClickListener {
+            val name:String=etxName.text.toString()
+            val clinic:String=etxClinic.text.toString()
+            val number:String=etxNumber.text.toString()
+            val edit = prefs.edit()
+            edit.putString("key1", "Nombre: $name,")
+            edit.putString("key2", " Clinica: $clinic,")
+            edit.putString("key3", " N° de vacuna: $number")
+            edit.apply()
+            Alerta("Hemos guardado los datos")
+
+        }
+        show.setOnClickListener {
+            val myprefs = prefs.getString("key1", "Nombre: No hay nombre registro ")
+            val myprefs1 = prefs.getString("key2", "Clinica: No hay registro ")
+            val myprefs2 = prefs.getString("key3", "N° de vacuna: No hay nombre registro ")
+
+
+            Alerta(myprefs.toString()+myprefs1.toString()+myprefs2.toString())
+
+        }
+
+
+
+
+
+    }
+    private fun Alerta(message: String){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Registro")
+        builder.setMessage(message)
+        val dialog = builder.create()
+        dialog.show()
 
     }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
